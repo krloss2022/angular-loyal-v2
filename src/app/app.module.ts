6e7736aa1ab9773d98a298faad157a99ec74e5be
carlos horacio lopez <krloss@gmail.com>
2022-11-18 08:54:04 -0300

    add bootstrap form + ractive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +30,8 @@ import { ResultadosComponent } from './resultados/resultados.component'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     LoginService
--- src/app/resultados/resultados.component.ts
+++ src/app/resultados/resultados.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../user.service';
 import { User } from './../product-detail/user-dto';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-resultados',
@@ -10,15 +11,27 @@ import { User } from './../product-detail/user-dto';
 export class ResultadosComponent implements OnInit {
 
   user?:User;
+  userForm: FormGroup = this.formBuilder.group(
+    {
+      id: [''],
+      email: [''],
+      firtName: ['']
+    }
+  );
 
   constructor(
-    private us:UserService
+    private us:UserService,
+    private  formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+
     this.us.getCurrentUser().subscribe(
       data=>  this.user = data
     );
   }
 
+  onSubmit(): void {
+
+  }
 }
